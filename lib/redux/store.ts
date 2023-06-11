894e23c9f8d085172cd82bfa9cc9253141eaa64f
Antonette Caldwell <acald.nebula@gmail.com>
2023-06-11 13:13:35 -0500

    Added counter example for reference

--- lib/redux/store.ts
+++ lib/redux/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./features/counter/counterSlice";
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        counter: counterReducer,
+    },
     devTools: process.env.NODE_ENV !== "production"
 })
 
